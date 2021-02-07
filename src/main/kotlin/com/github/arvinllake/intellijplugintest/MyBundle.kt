145f0235939148e56fee8bb2d862498cde1a93d1
GitHub Action <action@github.com>
2021-02-07 03:09:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/arvinllake/intellijplugintest/MyBundle.kt
+++ src/main/kotlin/com/github/arvinllake/intellijplugintest/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.arvinllake.intellijplugintest
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/arvinllake/intellijplugintest/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.arvinllake.intellijplugintest.services
+
+import com.github.arvinllake.intellijplugintest.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
